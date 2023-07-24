1b7b9a96b6af82df6071128a97cd2e33df8fe997
twomoonsbeat1 <twomoonsbeat@gmail.com>
2023-07-24 11:00:19 +0530

    Changed Some Code

--- Nova.py
+++ Nova.py
@@ -1,4 +1,4 @@
-from llama_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper, ServiceContext
+from llama_index import SimpleDirectoryReader, GPTListIndex, readers, GPTVectorStoreIndex, LLMPredictor, PromptHelper, ServiceContext
 from langchain import OpenAI
 import sys
 import os
@@ -7,7 +7,7 @@ from functions import *
 def construct_index(directory_path):
     max_input_size = 4096
     num_outputs = 2000
-    max_chunk_overlap = 20
+    max_chunk_overlap = 0.2
     chunk_size_limit = 600
     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
     llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.5, model_name="text-davinci-003", max_tokens=num_outputs))
@@ -15,27 +15,27 @@ def construct_index(directory_path):
     documents = SimpleDirectoryReader(directory_path).load_data()
 
     service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-    index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
+    index = GPTVectorStoreIndex.from_documents(documents, service_context=service_context)
 
     index.save_to_disk('index.json')
 
     return index
 
 def ask_ai():
-    index = GPTSimpleVectorIndex.load_from_disk('index.json')
+    index = GPTVectorStoreIndex.load_from_disk('index.json')
     print("Nova>> Hi I am Nova, your personal assistant")
     play_text_as_sound("Hi I am Nova, your personal assistant")
     print("Nova>> Please enter/ask something: ")
     while True:
         query = input("You>> ")
         if query.lower() == 'reconstruct_module':
-            construct_index('construct_index/data')
+            construct_index('data')
         else:
             response = index.query(query)
             if response == '':
                 continue
             print(f"Nova>> {response.response}")
             play_text_as_sound(response.response)
-os.environ["OPENAI_API_KEY"] = "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"
-construct_index("context_data/data")
+os.environ["OPENAI_API_KEY"] = "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"
+construct_index("data")
 ask_ai()
